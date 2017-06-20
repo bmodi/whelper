b5ce266764739124f809804df955b33c39ce7555
bmodi <bsmodi32@gmail.com>
2017-06-20 15:25:24 -0400

    Removed desktop files and build

--- src/test/java/ca/svarb/whelper/test/BaseTest.java
+++ src/test/java/ca/svarb/whelper/test/BaseTest.java
@@ -4,17 +4,14 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
-import ca.svarb.whelper.gui.TextImageLoaderTest;
-
 public class BaseTest {
 
 	protected Properties testProperties;
 
 	protected Properties getTestProperties() throws IOException {
-		InputStream inStream = TextImageLoaderTest.class.getClassLoader().getResourceAsStream("test.properties");
+		InputStream inStream = BaseTest.class.getClassLoader().getResourceAsStream("test.properties");
 		Properties testProperties = new Properties();
 		testProperties.load(inStream);
 		return testProperties;
 	}
-
 }
